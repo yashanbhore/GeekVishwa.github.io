812c2b23afff56ad172729ae54a49fab92f47e7f
Yash <yanbhore@gmail.com>
2023-10-24 13:21:17 +0530

    Insta and Linkedin Links Added

--- src/app/team/data.js
+++ src/app/team/data.js
@@ -1,6 +1,8 @@
 export default [
     {
         core: [
+
+
             {
                 img: "/assets/Team/Pratham Patil.png",
                 title: "Pratham Patil ",
@@ -8,22 +10,23 @@ export default [
                 instaLink:"https://instagram.com/_pratham18_?igshid=MzNlNGNkZWQ4Mg==",
                 linkedinLink:"https://www.linkedin.com/in/pratham-patil-4826a422b",
                 team: 'core',
-
             },
             {
                 img: "/assets/Team/AmanKasat - B2_33 Aman Kasat.jpg",
                 title: "Aman Kasat  ",
                 pos: "Vice President",
+		instaLink:"https://www.instagram.com/aman.kasat_",
+                linkedinLink:"https://www.linkedin.com/in/amankasat7/",
                 team: 'core',
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
             },
 
             {
                 img: "/assets/Team/RENUKA - L_1276_RENUKA WANKHADE.jpg",
                 title: "Renuka Wankhade",
                 pos: "General Manager",
+		instaLink:"https://instagram.com/renuuka.a?igshid=OGQ5ZDc2ODk2ZA==",
+                linkedinLink:"https://www.linkedin.com/in/renuka-wankhade-4a44bb1a6",
                 team: "core",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
             },
 
         ],
@@ -33,19 +36,23 @@ export default [
                 img: "/assets/Team/IMG20230714184124 - 711_KANHAIYA.jpg",
                 title: "Kanhaiya Sulay ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://www.instagram.com/kanhaiyasulay/",
+                linkedinLink:"https://www.linkedin.com/in/kanhaiya-sulay-42263a256/",
+		
             },
             {
                 img: "/assets/Team/Karan.jpg - 4022_KARAN _GULVE.jpg",
                 title: "Karan Gulve",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://www.instagram.com/karangulve_4342/#",
+                linkedinLink:"https://www.linkedin.com/in/karan-gulve-541716257",
             },
             {
                 img: "/assets/1.png",
                 title: "Heramb Patil ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/heramb_patil_?igshid=OGQ5ZDc2ODk2ZA==",
+                linkedinLink:"https://www.linkedin.com/in/heramb-patil-15a583253?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
             },
 
         ],
@@ -54,19 +61,22 @@ export default [
                 img: "/assets/Team/IMG_20230909_121357 - 545_VAISHNAVI PATIL.jpg",
                 title: "Vaishnavi Patil",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/vaishnavi_patil_219?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+                linkedinLink:"https://www.linkedin.com/in/vaishnavi-patil-8bb58525b",
             },
             {
                 img: "/assets/Team/IMG_20230902_084214 - 2061_SANSKRUTI BURKULE.jpg",
                 title: "Sanskruti Burkule",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/sanskrutii_56?igshid=MzNlNGNkZWQ4Mg==",
+                linkedinLink:"https://www.linkedin.com/in/sanskruti-burkule-6a64ba258",
             },
             {
                 img: "/assets/1.png",
                 title: "Isha Varade ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://www.instagram.com/invites/contact/?i=il95h35v0cpc&utm_content=p35zhmx",
+                linkedinLink:"https://www.linkedin.com/in/isha-varade-b5643625a",
             },
 
         ],
@@ -76,19 +86,22 @@ export default [
                 img: "/assets/Team/20230829_180430 - 371_Ojas_Deshpande.jpg",
                 title: "Ojas Deshpande ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/real_ojasd?igshid=OGQ5ZDc2ODk2ZA==",
+                linkedinLink:"https://www.linkedin.com/in/ojas-deshpande-aa1b2425a",
             },
             {
                 img: "/assets/Team/IMG-20230910-WA0023 - C1_14_Kshitij Habbu.jpg",
                 title: "Kshitij Habbu",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/_kshitij25704_?igshid=NzZlODBkYWE4Ng==",
+                linkedinLink:"https://www.linkedin.com/in/kshitij-habbu-23936225b",
             },
             {
                 img: "/assets/Team/Pournima Bhosale_1 - C_POURNIMA BHOSALE.jpg",
                 title: "Pournima Bhosale",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/pournima_bhosale2710?utm_source=qr&igshid=YzU1NGVlODEzOA==",
+                linkedinLink:"https://www.linkedin.com/in/pournima-bhosale-58599825a",
             },
 
         ],
@@ -98,13 +111,15 @@ export default [
                 img: "/assets/Team/Atharva - 5_ATHARVA_ ATTERKAR.jpg",
                 title: "Atharva Atterkar ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/atharva_atterkar?igshid=OGQ5ZDc2ODk2ZA==",
+                linkedinLink:"https://www.linkedin.com/in/pratham-patil-4826a422b",
             },
             {
                 img: "/assets/Team/IMG_20230909_085142 - SY_A2_Rajat Patel.jpg",
                 title: "Rajat Patel ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/_pratham18_?igshid=MzNlNGNkZWQ4Mg==",
+                linkedinLink:"https://www.linkedin.com/public-profile/settings?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_self_edit_contact-info%3BT8mVXtFpQv6yodJpcNwAnw%3D%3D",
             },
         ],
 
@@ -113,35 +128,39 @@ export default [
                 img: "/assets/Team/Photo - DAMJI YASHRAJ.jpg",
                 title: "Yashraj Damji",
                 pos: "Documentation Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/yashraj_damji?utm_source=qr&igshid=MzNlNGNkZWQ4Mg==",
+                linkedinLink:"https://www.linkedin.com/in/yashraj-damji-74b5b6245",
             },
             {
                 img: "/assets/1.png",
                 title: "Om Gole",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/om_gole125?utm_source=qr&igshid=YzU1NGVlODEzOA%3D%3D",
+                linkedinLink:"https://www.linkedin.com/in/om-gole-4aa828260",
             },
         ],
 
         content: [
-           
-            {
-                img: "/assets/Team/IMG-20230907-WA0013 - 576 YASH SAROLKAR.jpg",
-                title: "Yash Sarolkar ",
-                pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
-            },
             {
                 img: "/assets/Team/SAVE_20230907_155154 - TY_B_ 34_Shivani Moghe.jpg",
                 title: "Shivani Moghe",
                 pos: "Content Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/_.shivani._1703?igshid=OGQ5ZDc2ODk2ZA==",
+                linkedinLink:"https://www.linkedin.com/in/shivani-moghe-a06472248",
+            },
+            {
+                img: "/assets/Team/IMG-20230907-WA0013 - 576 YASH SAROLKAR.jpg",
+                title: "Yash Sarolkar ",
+                pos: "Associate Head",
+		instaLink:"https://instagram.com/yash__sarolkar?igshid=MzMyNGUyNmU2YQ==",
+                linkedinLink:"https://www.linkedin.com/in/yash-sarolkar-60284b25b",
             },
             {
                 img: "/assets/Team/IMG_20230222_172423 - C2_43_HARSHAL PATIL.jpg",
                 title: "Harshal Patil",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/_hhaarrsshhaall?igshid=YTQwZjQ0NmI0OA==",
+                linkedinLink:"https://www.linkedin.com/in/harshal-patil-88b58825b/",
             },
 
         ],
@@ -151,19 +170,22 @@ export default [
                 img: "/assets/1.png",
                 title: "Smit Ramteke",
                 pos: "Publicity Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://www.instagram.com/smit.__.ramteke/#",
+                linkedinLink:"https://www.linkedin.com/in/smit-ramteke-4b60b5232",
             },
             {
                 img: "/assets/Team/IMG20230608210500 - N_1434_SIDDHESH MUNDHE.jpg",
                 title: "Siddhesh Mundhe  ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/siddh_15_5?utm_source=qr&igshid=MzNlNGNkZWQ4Mg%3D%3D",
+                linkedinLink:"https://www.linkedin.com/in/siddhesh-mundhe-5b0071263",
             },
             {
                 img: "/assets/Team/IMG_20230222_172423 - C2_43_HARSHAL PATIL.jpg",
-                title: "           Harsh Mane ",
+                title: "Harsh Mane ",
                 pos: "Associate Head",
-                text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+		instaLink:"https://instagram.com/mane30harsh?igshid=MzMyNGUyNmU2YQ==",
+                linkedinLink:"https://www.linkedin.com/in/harsh-mane-279654260",
             },
 
         ],
