bca7712743b4d81f9cf1f9b8f21a2c471b3f8d46
Adinath Shelke <shelkeadinath3@gmail.com>
2023-10-24 12:22:02 +0530

    added fireworks but responsiveness is remaining (#20)

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -27,144 +27,151 @@ const Footer = () => {
     }, 500)
      
     if (pathname == "/") {
-      var c = document.getElementById("fire-works");
-      var ctx = c.getContext("2d");
+      let canvas, width, height, ctx;
+      let fireworks = [];
+      let particles = [];
 
-      var cwidth, cheight;
-      var shells = [];
-      var pass = [];
-
-      var colors = [
-        "#FF5252",
-        "#FF4081",
-        "#E040FB",
-        "#7C4DFF",
-        "#536DFE",
-        "#448AFF",
-        "#40C4FF",
-        "#18FFFF",
-        "#64FFDA",
-        "#69F0AE",
-        "#B2FF59",
-        "#EEFF41",
-        "#FFFF00",
-        "#FFD740",
-        "#FFAB40",
-        "#FF6E40",
-      ];
-
-      window.onresize = function () {
-        reset();
-      };
-      reset();
-      function reset() {
-        cwidth = window.innerWidth;
-        cheight = window.innerHeight;
-        c.width = cwidth;
-        c.height = cheight;
+      function setup() {
+        canvas = document.getElementById("canvas");
+        setSize(canvas);
+        ctx = canvas.getContext("2d");
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, width, height);
+        fireworks.push(new Firework(Math.random() * (width - 200) + 100));
+        window.addEventListener("resize", windowResized);
+        document.addEventListener("click", onClick);
       }
 
-      function newShell() {
-        var left = Math.random() > 0.5;
-        var shell = {};
-        shell.x = 1 * left;
-        shell.y = 1;
-        shell.xoff = (0.01 + Math.random() * 0.007) * (left ? 1 : -1);
-        shell.yoff = 0.01 + Math.random() * 0.007;
-        shell.size = Math.random() * 6 + 3;
-        shell.color = colors[Math.floor(Math.random() * colors.length)];
+      setTimeout(setup, 1);
 
-        shells.push(shell);
+      function loop() {
+        ctx.globalAlpha = 0.1;
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, width, height);
+        ctx.globalAlpha = 1;
+
+        for (let i = 0; i < fireworks.length; i++) {
+          let done = fireworks[i].update();
+          fireworks[i].draw();
+          if (done) fireworks.splice(i, 1);
+        }
+
+        for (let i = 0; i < particles.length; i++) {
+          particles[i].update();
+          particles[i].draw();
+          if (particles[i].lifetime > 80) particles.splice(i, 1);
+        }
+
+        if (Math.random() < 1 / 60)
+          fireworks.push(new Firework(Math.random() * (width - 200) + 100));
+      }
+      setInterval(loop, 1 / 60);
+      //setInterval(loop, 100/60);
+      class Particle {
+        constructor(x, y, col) {
+          this.x = x;
+          this.y = y;
+          this.col = col;
+          this.vel = randomVec(2);
+          this.lifetime = 0;
+        }
+
+        update() {
+          this.x += this.vel.x;
+          this.y += this.vel.y;
+          this.vel.y += 0.02;
+          this.vel.x *= 0.99;
+          this.vel.y *= 0.99;
+          this.lifetime++;
+        }
+
+        draw() {
+          ctx.globalAlpha = Math.max(1 - this.lifetime / 80, 0);
+          ctx.fillStyle = this.col;
+          ctx.fillRect(this.x, this.y, 2, 2);
+        }
       }
 
-      function newPass(shell) {
-        var pasCount = Math.ceil(Math.pow(shell.size, 2) * Math.PI);
+      class Firework {
+        constructor(x) {
+          this.x = x;
+          this.y = height;
+          this.isBlown = false;
+          this.col = randomCol();
+        }
 
-        for (i = 0; i < pasCount; i++) {
-          var pas = {};
-          pas.x = shell.x * cwidth;
-          pas.y = shell.y * cheight;
-
-          var a = Math.random() * 4;
-          var s = Math.random() * 10;
-
-          pas.xoff = s * Math.sin((5 - a) * (Math.PI / 2));
-          pas.yoff = s * Math.sin(a * (Math.PI / 2));
-
-          pas.color = shell.color;
-          pas.size = Math.sqrt(shell.size);
-
-          if (pass.length < 1000) {
-            pass.push(pas);
+        update() {
+          this.y -= 3;
+          if (this.y < 350 - Math.sqrt(Math.random() * 500) * 40) {
+            this.isBlown = true;
+            for (let i = 0; i < 60; i++) {
+              particles.push(new Particle(this.x, this.y, this.col));
+            }
           }
+          return this.isBlown;
+        }
+
+        draw() {
+          ctx.globalAlpha = 1;
+          ctx.fillStyle = this.col;
+          ctx.fillRect(this.x, this.y, 2, 2);
         }
       }
 
-      var lastRun = 0;
+      function randomCol() {
+        var letter = "0123456789ABCDEF";
+        var nums = [];
 
-      function Run() {
-        var dt = 1;
-        if (lastRun != 0) {
-          dt = Math.min(50, performance.now() - lastRun);
-        }
-        lastRun = performance.now();
-
-        ctx.clearRect(0, 0, cwidth, cheight);
-        ctx.fillStyle = "rgba(0,0,0,0.25)";
-        ctx.fillRect(0, 0, cwidth, cheight);
-
-        if (shells.length < 10 && Math.random() > 0.96) {
-          newShell();
+        for (var i = 0; i < 3; i++) {
+          nums[i] = Math.floor(Math.random() * 256);
         }
 
-        for (let ix in shells) {
-          var shell = shells[ix];
-
-          ctx.beginPath();
-          ctx.arc(
-            shell.x * cwidth,
-            shell.y * cheight,
-            shell.size,
-            0,
-            2 * Math.PI
-          );
-          ctx.fillStyle = shell.color;
-          ctx.fill();
-
-          shell.x -= shell.xoff;
-          shell.y -= shell.yoff;
-          shell.xoff -= shell.xoff * dt * 0.001;
-          shell.yoff -= (shell.yoff + 0.2) * dt * 0.00005;
-
-          if (shell.yoff < -0.005) {
-            newPass(shell);
-            shells.splice(ix, 1);
-          }
+        let brightest = 0;
+        for (var i = 0; i < 3; i++) {
+          if (brightest < nums[i]) brightest = nums[i];
         }
 
-        for (let ix in pass) {
-          var pas = pass[ix];
-
-          ctx.beginPath();
-          ctx.arc(pas.x, pas.y, pas.size, 0, 2 * Math.PI);
-          ctx.fillStyle = pas.color;
-          ctx.fill();
-
-          pas.x -= pas.xoff;
-          pas.y -= pas.yoff;
-          pas.xoff -= pas.xoff * dt * 0.001;
-          pas.yoff -= (pas.yoff + 5) * dt * 0.0005;
-          pas.size -= dt * 0.002 * Math.random();
-
-          if (pas.y > cheight || pas.y < -50 || pas.size <= 0) {
-            pass.splice(ix, 1);
-          }
+        brightest /= 255;
+        for (var i = 0; i < 3; i++) {
+          nums[i] /= brightest;
         }
-        requestAnimationFrame(Run);
+
+        let color = "#";
+        for (var i = 0; i < 3; i++) {
+          color += letter[Math.floor(nums[i] / 16)];
+          color += letter[Math.floor(nums[i] % 16)];
+        }
+        return color;
       }
-      setInterval(() => {
-        Run();
-      }, 500);
+
+      function randomVec(max) {
+        let dir = Math.random() * Math.PI * 2;
+        let spd = Math.random() * max;
+        return { x: Math.cos(dir) * spd, y: Math.sin(dir) * spd };
+      }
+
+      function setSize(canv) {
+        canv.style.width = innerWidth + "px";
+        canv.style.height = innerHeight + "px";
+        width = innerWidth;
+        height = innerHeight;
+
+        canv.width = innerWidth * window.devicePixelRatio;
+        canv.height = innerHeight * window.devicePixelRatio;
+        canvas
+          .getContext("2d")
+          .scale(window.devicePixelRatio, window.devicePixelRatio);
+      }
+
+      function onClick(e) {
+        fireworks.push(new Firework(e.clientX));
+      }
+
+      function windowResized() {
+        setSize(canvas);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, width, height);
+      }
     }
   }, [])
 
--- src/component/Prize.jsx
+++ src/component/Prize.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from "react";
 
 const Prize = () => {
-    return (
-        <div className="prize-main h-[40%] flex justify-center mb-[3rem] ">
-            <canvas id="fire-works"></canvas>
-            <div className='h-full w-[50%] text-white flex  flex-col items-center prize-inner rounded-tl-[40px] rounded-br-[40px] pt-[2rem]'>
-                <div className="prize-Title  sponsor-main-title gfg-gradient">
-                    <span>Exciting Prizes ahead!!</span>
-                </div>
-                <div className='flex justify-center items-center h-[100%] flex-col'>
-                    <span className='text-[20px] font-800'>
-                        Win Prizes Upto
-                    </span>
-                    <span className='text-[50px] font-black'>5000$</span>
-                    <div className="prize-logo w-[150px] mt-[1rem]">
-                        <img src="/assets/coin.png" alt="" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="prize-main h-[60%] flex justify-center mb-[3rem]">
+      <canvas id="canvas"></canvas>
+      <div className="h-full w-[50%] sticky text-white flex  flex-col items-center prize-inner rounded-tl-[40px] rounded-br-[40px] pt-[2rem]">
+        <div className="prize-Title  sponsor-main-title gfg-gradient">
+          <span>Exciting Prizes ahead!!</span>
         </div>
-    )
-}
+        <div className="flex justify-center items-center h-[100%] flex-col pb-[2rem]">
+          <span className="text-[20px] font-800">Win Prizes Upto</span>
+          <span className="text-[50px] font-black">5000$</span>
+          <div className="prize-logo w-[10rem] mt-[1rem] flex justify-center">
+            <img src="/assets/coin.png" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Prize
+export default Prize;
