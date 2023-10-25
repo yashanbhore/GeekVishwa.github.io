ba78501e65bc5a86b6f79868ae6d66bdb62fbcaf
Aryan <aryanitinpatil@gmail.com>
2023-10-25 21:56:09 +0530

    fixed prize section

--- src/component/Prize.jsx
+++ src/component/Prize.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Prize = () => {
   return (
-    <div className="prize-main h-[60%] flex justify-center mb-[3rem]">
+    <div className="prize-main h-[60%] flex justify-center mb-[6rem]">
       <canvas id="canvas"></canvas>
       <div className="h-full w-[50%] sticky text-white flex  flex-col items-center prize-inner rounded-tl-[40px] rounded-br-[40px] pt-[2rem]">
         <div className="prize-Title flex sponsor-main-title gfg-gradient">
@@ -11,10 +11,8 @@ const Prize = () => {
         <div className="flex justify-center items-center h-[100%] flex-col pb-[2rem]">
           <span className="text-[20px] font-800">Win Prizes Upto</span>
           <span className="text-[50px] font-black dollar">5000$</span>
-          <div className="prize-logo w-[20rem] mt-[1rem] mb-[2rem] flex justify-center">
-            <img src="/assets/prizes.png" alt="" />
-            {/* <video src="/assets/prize.mp4"/> */}
-            
+          <div className="prize-logo mt-[1rem] mb-[2rem] w-[50%] flex justify-center">
+            <img src="/assets/prizes.png" alt="" />            
           </div>
         </div>
       </div>
