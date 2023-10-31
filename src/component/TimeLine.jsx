b577dee51860ff33e22f4915f888e7e4d390a8f8
Aryan Patil <95161886+AryanP45@users.noreply.github.com>
2023-10-31 11:26:47 +0530

    added domain button (#32)

--- src/component/TimeLine.jsx
+++ src/component/TimeLine.jsx
@@ -13,6 +13,7 @@ import EventAvailableIcon from '@mui/icons-material/EventAvailable';
 
 import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import Typography from '@mui/material/Typography';
+import { Button } from '@mui/material';
 const TimeLine = () => {
   return (
     <div className='mb-[3rem] '>
@@ -96,6 +97,21 @@ const TimeLine = () => {
         </TimelineContent>
       </TimelineItem>
     </Timeline>
+
+    <div className='flex justify-center text-[#14c941]  '>
+      {/* custom button */}
+      <a href="/assets/domains.pdf" class="relative inline-flex items-center justify-center px-6 py-3 overflow-hidden font-bold text-white rounded-md shadow-2xl group">
+<span class="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100 border-6"></span>
+<span class="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
+<span class="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
+<span class="absolute bottom-0 left-0 w-4 h-full bg-gradient-to-r from-white to-transparent opacity-5"></span>
+<span class="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
+<span class="absolute inset-0 w-full h-full border border-white border-6 rounded-md opacity-10"></span>
+<span class="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full border-8 group-hover:w-56 group-hover:h-56 opacity-5"></span>
+<span class="relative">VIEW DOMAINS</span>
+</a>
+
+    </div>
     </div>
   )
 }
