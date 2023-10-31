292736d687744b6cebde947b721967b52493081c
Aryan <aryanitinpatil@gmail.com>
2023-11-01 02:16:21 +0530

    added new component

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@ import otherdata from '../../public/assets/sponsor/other_data'
 import Banner from '@/component/Banner'
 import TimeLine from '@/component/TimeLine'
 import Prize from '@/component/Prize'
+import AboutGeekVishwa from '@/component/AboutGeekVishwa'
 
 
 export default function Home() {
@@ -17,15 +18,20 @@ export default function Home() {
   const gfghead="About us"
   const gfgdesc= "GeeksforGeeks Student Chapters are university-based community chapters creating a coding culture for students interested in Computer Science and other core technical competencies. By joining the GeeksforGeeks student Chapters, students will have the opportunity to:Get an internship at GeeksforGeeks. Grow their knowledge in a peer-to-peer learning environment.Learn to build effective and optimized solutions for local businesses and their community as per the latest technology trends.Great learning and networking experience."
 
-  const gwhead="what is GeekVishwa"
-  const gwgdesc= "GeekVishwa Hackathon is planned to be one of the largest hybrid (Offline-Online) hackathons in Pune, Organized by GeeksForGeeks Student Chapter VIIT.It is event of unprecedented scale, opportunity, and rewards, where we intent to attract and showcase the brightest talents across the plethora of engineering colleges in Pune.This event is designed to foster creativity, collaboration, and innovation among participants. It offers an excellent opportunity for participants to showcase their skills, network with peers, and potentially win prizes and recognition for their work.During the event, participants will work in teams to develop innovative ideas that can solve real-world problems. They will be encouraged to use their creativity, technical skills, and critical thinking abilities to develop solutions that are both practical and effective."
+  const gwhead=" What is GeekVishwa"
+  const p1 = " GeekVishwa Hackathon in Pune, organized by GeeksForGeeks Student Chapter VIIT, is a significant hybrid (Offline-Online) event."
+  const p2 = " It aims to attract top talents from engineering colleges, encouraging creativity, collaboration, and innovation."
+  const p3 = " Participants work in teams to develop practical solutions for real-world problems, utilizing their technical skills and critical thinking abilities."
+  const p4 = "The event offers opportunities for recognition and prizes, fostering a competitive and rewarding atmosphere."
+
+  // const gwgdesc= "GeekVishwa Hackathon is planned to be one of the largest hybrid (Offline-Online) hackathons in Pune, Organized by GeeksForGeeks Student Chapter VIIT.It is event of unprecedented scale, opportunity, and rewards, where we intent to attract and showcase the brightest talents across the plethora of engineering colleges in Pune.This event is designed to foster creativity, collaboration, and innovation among participants. It offers an excellent opportunity for participants to showcase their skills, network with peers, and potentially win prizes and recognition for their work.During the event, participants will work in teams to develop innovative ideas that can solve real-world problems. They will be encouraged to use their creativity, technical skills, and critical thinking abilities to develop solutions that are both practical and effective."
 
   return (
     <div className='content'>
       <Hero />
       <AboutGFG head={gfghead} desc={gfgdesc} />
       <div id="event-section">
-      <AboutGFG head={gwhead} desc={gwgdesc} />
+      <AboutGeekVishwa head={gwhead} p1={p1} p2={p2} p3={p3} p4={p4} />
       </div>
       <Banner/>
       <Prize/>
