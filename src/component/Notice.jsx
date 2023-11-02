100beaf9f8ba2dfac837fc226c77b19b9cfd5f5f
Aryan <aryanitinpatil@gmail.com>
2023-11-03 00:42:35 +0530

    Update Notice.jsx

--- src/component/Notice.jsx
+++ src/component/Notice.jsx
@@ -12,7 +12,8 @@ const Notice = () => {
         <Stack sx={{ width: '100%' }} spacing={2} className='flex justify-center items-center' >
       <Alert variant="outlined" severity="info" className='note-body'  >
         <AlertTitle className='note-title'>Note:</AlertTitle>
-        Individual registration won't be considered !
+        {/* Individual registration won't be considered ! */}
+        No individual registrations are allowed, you have to register in a team of 3 or 4 members only.
       </Alert>
     </Stack>
     </div>
